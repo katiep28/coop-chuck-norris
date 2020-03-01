432c4d2ce7cbdc9c9a98e992d15ffb20c88740c4
Katie Postle <katiepostle@hotmail.co.uk>
2020-03-01 20:40:54 +0000

    background picture set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RandomJoke from './components/RandomJoke';
 
 
 const axios = require('axios');
@@ -11,25 +12,27 @@ class App extends React.Component {
   }
   render() {
 
-   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Katies React App
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <div>
+        <div className="row">
+          <div className="col-12 col-lg-12">
+            <h1>Chuck Norris Jokes</h1>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-6 col-lg-6">
+            <RandomJoke/>
+                  {/* searchCocktailFunc={this.searchCocktailByName}
+                  showRecipeFunc={this.showRecipe}
+                  key="0" /> */}
+          </div>
+          <div className="col-6 col-lg-6">
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/RandomJoke.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+class App extends React.Component {
+
+  state = {
+  }
+  render() {
+
+    return (
+      <div>
+        <div className="row">
+          <div className="col-12 col-lg-12">
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
