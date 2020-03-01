41835c407ad5f71a9d54d51e0b1c4ca6fde98af3
Katie Postle <katiepostle@hotmail.co.uk>
2020-03-01 19:32:58 +0000

    Bootstrap Added

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Katies React
+          Katies React App
         </a>
       </header>
     </div>
