75a63215ea8ae88c264c528aa60c2518f57aa007
Katie Postle <katiepostle@hotmail.co.uk>
2020-03-05 17:12:59 +0000

    Updates

---
+++ precache-manifest.52d65a340153703c0e527d7682a9e624.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bfd89c30570ae98402cdf95514c30f0",
+    "url": "/coop-chuck-norris/index.html"
+  },
+  {
+    "revision": "e4dee91081ffce3e8d10",
+    "url": "/coop-chuck-norris/static/css/main.ea6d4da3.chunk.css"
+  },
+  {
+    "revision": "b09f61c6223a65b3a57f",
+    "url": "/coop-chuck-norris/static/js/2.31afb66c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/coop-chuck-norris/static/js/2.31afb66c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4dee91081ffce3e8d10",
+    "url": "/coop-chuck-norris/static/js/main.aa9233c2.chunk.js"
+  },
+  {
+    "revision": "5731c5700310dedcf651",
+    "url": "/coop-chuck-norris/static/js/runtime-main.2abdb413.js"
+  },
+  {
+    "revision": "fcc5a2c4a309d367a036eafca8e06811",
+    "url": "/coop-chuck-norris/static/media/norris.fcc5a2c4.jpg"
+  }
+]);
