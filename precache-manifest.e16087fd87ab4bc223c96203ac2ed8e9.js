e4c013a3f10f6c51a721d1418989e0f2d6a79460
Katie Postle <katiepostle@hotmail.co.uk>
2020-03-05 18:51:32 +0000

    Updates

--- precache-manifest.e16087fd87ab4bc223c96203ac2ed8e9.js
+++ precache-manifest.e16087fd87ab4bc223c96203ac2ed8e9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2bfd89c30570ae98402cdf95514c30f0",
+    "revision": "8a8787dc849f2a8ebe84d5274888107a",
     "url": "/coop-chuck-norris/index.html"
   },
   {
-    "revision": "e4dee91081ffce3e8d10",
+    "revision": "3983d7acaaacd838ccf5",
     "url": "/coop-chuck-norris/static/css/main.ea6d4da3.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/coop-chuck-norris/static/js/2.31afb66c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e4dee91081ffce3e8d10",
-    "url": "/coop-chuck-norris/static/js/main.aa9233c2.chunk.js"
+    "revision": "3983d7acaaacd838ccf5",
+    "url": "/coop-chuck-norris/static/js/main.6bb8bc8a.chunk.js"
   },
   {
     "revision": "5731c5700310dedcf651",
